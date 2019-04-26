7f31916c92ed677889ecd4ada1b9ff978960eab6
jonathasmedina <emaildojonathas@gmail.com>
2019-04-26 11:21:40 -0400

    -

--- app/src/main/java/com/example/responsiveapp/MainActivity.java
+++ app/src/main/java/com/example/responsiveapp/MainActivity.java
@@ -4,45 +4,57 @@ import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static android.content.res.Configuration.SCREENLAYOUT_SIZE_LARGE;
+import static android.content.res.Configuration.SCREENLAYOUT_SIZE_NORMAL;
+import static android.content.res.Configuration.SCREENLAYOUT_SIZE_SMALL;
+import static android.content.res.Configuration.SCREENLAYOUT_SIZE_XLARGE;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Configuration config = getResources().getConfiguration();
-        if (config.smallestScreenWidthDp >= 600)  //telas de no mínimo 600 - tablet
+
+        if ((config.screenLayout &  Configuration.SCREENLAYOUT_SIZE_MASK) == SCREENLAYOUT_SIZE_XLARGE)  //tablet
             setContentView(R.layout.activity_main_tablet);
-        else  if (config.smallestScreenWidthDp >= 480) //telefone, large screen
+        if ((config.screenLayout &  Configuration.SCREENLAYOUT_SIZE_MASK) == SCREENLAYOUT_SIZE_LARGE) //telefone, large screen
             setContentView(R.layout.activity_main_phone_large);
-        else if (config.smallestScreenWidthDp >= 320) //telefone, normal screen
+        if ((config.screenLayout &  Configuration.SCREENLAYOUT_SIZE_MASK) == SCREENLAYOUT_SIZE_NORMAL) //telefone, normal screen
             setContentView(R.layout.activity_main_phone_normal);
-        else
-            setContentView(R.layout.activity_main);
+        if ((config.screenLayout &  Configuration.SCREENLAYOUT_SIZE_MASK) == SCREENLAYOUT_SIZE_SMALL)
+            setContentView(R.layout.activity_main__phone_small);
+
         //para detectar paisagem
-        //  if (config.orientation == Configuration.ORIENTATION_LANDSCAPE)
-        //    setContentView(R.layout.activity_main_landscape);
+
+        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE)
+            setContentView(R.layout.activity_main_landscape);
 
     }
 }
 
 
 
-/*Phones:
-2,7 small;
-3,4 small;
-3,7 normal;
-4,95 normal;
-4,99 normal;
-5,0 large;
-6,5 large;
-6,9 large;
+/*Phones - telas e tamanhos:
+VALORES APROXIMADOS, O QUE CONTA REALMENTE É A RESOLUÇÃO/DENSIDADE DOS PIXELS
+
+2,7" small;
+3,4" small;
+3,7" normal;
+4,95" normal;
+4,99" normal;
+5,99" normal;
+6,0" large;
+6,3" large;
+6,5" large;
+6,9" large;
 
 Tablets:
-7,0 large;
-7,5 xlarge;
-8,86 xlarge;
-9,94 xlarge;
-10,1 xlarge;
+7,0" large;
+7,5" xlarge;
+8,86" xlarge;
+9,94" xlarge;
+10,1" xlarge;
 
 	1.
 xlarge screens are at least 960dp x 720dp.
@@ -54,4 +66,4 @@ normal screens are at least 470dp x 320dp.
 small screens are at least 426dp x 320dp.
 
 
-para testar: segundo argumento (largura) -> config.smallestScreenWidthDp >= 600*/
+*/
